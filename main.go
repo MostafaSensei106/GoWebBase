e5bc7d0f5092a11241e735eaab281ffb48ed906e
MostafaSensei106 <wwmostafagmer100@gmail.com>
2025-12-04 01:42:49 +0200

    refactor(server): centralize server execution logic

--- main.go
+++ main.go
@@ -1,19 +1,9 @@
 package main
 
 import (
-	"fmt"
-	"log"
-	"net/http"
-
 	"github.com/MostafaSensei106/GoWebBase/server"
 )
 
 func main() {
-	server.RegisterRoutes()
-
-	fmt.Printf("Starting server at port 8080\n")
-	if err := http.ListenAndServe(":8080", nil); err != nil {
-		log.Fatal(err)
-	}
-
+	server.Execute()
 }
--- server/routes.go
+++ server/routes.go
@@ -1,13 +1,15 @@
 package server
 
 import (
+	"fmt"
+	"log"
 	"net/http"
 
 	"github.com/MostafaSensei106/GoWebBase/constants"
 	"github.com/MostafaSensei106/GoWebBase/logic/handlers"
 )
 
-func RegisterRoutes() {
+func Execute() {
 	// Static files
 	fileServer := http.FileServer(http.Dir(constants.StaticFolder))
 	http.Handle(constants.IndexRoute, fileServer)
@@ -15,4 +17,9 @@ func RegisterRoutes() {
 	// Handlers
 	http.HandleFunc(constants.FormRoute, handlers.FormHandler)
 	http.HandleFunc(constants.HelloRoute, handlers.HelloHandler)
+
+	fmt.Printf("Starting server at port 8080\n")
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal(err)
+	}
 }
